46db426c3175343a3df2fc89526f7f5c9ac0d9b3
Kevin <klambe17@gmail.com>
2017-11-17 15:49:48 +0000

    added video modal to playground

--- playground/js/test1.js
+++ playground/js/test1.js
@@ -1,25 +1,29 @@
-//
-// $(function () {
-//     $('[data-toggle="tooltip"]').tooltip()
-// })
-//
-// $(document).ready(function(){
-//     $('[data-toggle="tooltip"]').tooltip();
-//
-//     $("[rel='tooltip']").tooltip();
-//
-//     $('.thumbnail').hover(
-//         function(){
-//             $(this).find('.caption').slideDown(250); //.fadeIn(250)
-//         },
-//         function(){
-//             $(this).find('.caption').slideUp(250); //.fadeOut(205)
-//         }
-//     );
-// });
+// Add url parameter or replace if already exists.
+function addUrlParam(url, key, value) {
+    var newParam = key+"="+value;
+    var result = url.replace(new RegExp("(&|\\?)"+key+"=[^\&|#]*"), '$1' + newParam);
+    if (result === url) {
+        result = (url.indexOf("?") != -1 ? url.split("?")[0]+"?"+newParam+"&"+url.split("?")[1]
+            : (url.indexOf("#") != -1 ? url.split("#")[0]+"?"+newParam+"#"+ url.split("#")[1]
+                : url+'?'+newParam));
+    }
+    return result;
+}
 
-$('#panelPrecios [data-toggle="tooltip"]').tooltip({
-    animated: 'fade',
-    placement: 'bottom',
-    html: true
+// Add and remove autoplay on videos opened in modals
+$('.modal.videoModal').on('shown.bs.modal', function() {
+    var $iframe = $(this).find('iframe');
+    if ($iframe.length) {
+        var src = $iframe.attr('src');
+        var srcNew = addUrlParam(src, 'autoplay', '1');
+        $iframe.attr('src', srcNew);
+    }
+});
+$('.modal.videoModal').on('hide.bs.modal', function() {
+    var $iframe = $(this).find('iframe');
+    if ($iframe.length) {
+        var src = $iframe.attr('src');
+        var srcNew = addUrlParam(src, 'autoplay', '0');
+        $iframe.attr('src', srcNew);
+    }
 });
