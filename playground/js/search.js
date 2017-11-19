7442bf2933fc0799b3356468a4cace554d64a3b2
Kevin <klambe17@gmail.com>
2017-11-19 15:55:08 +0000

    added background color to accordion

---
+++ playground/js/search.js
@@ -0,0 +1,14 @@
+
+const pages = ['home', 'education', 'skills','exp'];
+
+function myFunction() {
+    var nameValue = document.getElementById("mySearch2").value;
+
+    // var x = document.getElementById("mySearch").placeholder;
+    // document.getElementById("demo").innerHTML = x;
+    alert(nameValue);
+    //window.location.href = 'www.google.ie';
+    window.location = "http://www.google.com";
+}
+
+// window.location.href = 'URL';
